a19f5dbae0e0c8164199fec719d368c7a1440c80
Ishmael <jaswged@gmail.com>
2020-01-09 16:41:38 -0600

    Add new form with javascript and endpoint to test model

--- app/app.py
+++ app/app.py
@@ -1,17 +1,21 @@
-# app/app.py
-
 # Common python package imports.
 from flask import Flask, jsonify, request, render_template
 import pickle
 import numpy as np
 import sys
+from io import BytesIO
+import time
+from fastai.vision import *
 
 # Initialize the app and set a secret_key.
 app = Flask(__name__)
 app.secret_key = 'something_secret'
 
 # Load the pickled model.
+defaults.device = torch.device('cpu')
 MODEL = pickle.load(open('dice.pkl', 'rb'))
+path = '.'
+learn = load_learner(path, file='dice.pkl')
 
 
 @app.route('/')
@@ -19,27 +23,56 @@ def docs():
     print("Root called", file=sys.stderr)
     return render_template('docs.html')
 
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    data = await request.form()
+    img_bytes = await (data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    prediction = learn.predict(img)[0]
+    return jsonify(prediction)
+
+
+@app.route('/a')
+def a():
+    print(path)
+    time.sleep(5)
+    print(path)
+    img = open_image('C:/Users/Jason/Pictures/d12.jpg')
+    pred_class, pred_idx, outputs = learn.predict(img)
+    print(pred_class, file=sys.stderr)
+    print(pred_class)
+    return render_template('docs.html')
+
+
 @app.route('/classify', methods=["POST"])
-def classify():
+async def classify(request):
     print("Hello world", file=sys.stderr)
     print("Classify called")
     print(request.json, file=sys.stderr)
 
-@app.route("/form2")
-def form2(request):
-    return HTMLResponse(
-        """
-        <form action="/upload" method="post" enctype="multipart/form-data">
-            Select image to upload:
-            <input type="file" name="file">
-            <input type="submit" value="Upload Image">
-        </form>
-        Or submit a URL:
-        <form action="/classify-url" method="get">
-            <input type="url" name="url">
-            <input type="submit" value="Fetch and analyze image">
-        </form>
-    """)
+    # Get Request Form data
+    data = await request.form()
+    # request.form()
+    print(data)
+
+    # Get image as bytes
+    img_bytes = await (data['file'].read())
+    bytes = await(data["file"].read())
+    print(bytes)
+
+    img_ = open_image(BytesIO(img_bytes))
+    img = open_image(BytesIO(bytes))
+
+    prediction = learn.predict(img_)[0]
+    pred_class, pred_idx, outputs = learn.predict(img)
+
+    print(prediction)
+    print(pred_class)
+    print(outputs)
+
+    return jsonify(prediction)
+
 
 @app.route("/form")
 def form():
@@ -47,46 +80,23 @@ def form():
     return render_template('form.html')
 
 
-@app.route("/upload", methods=["POST"])
-async def upload():
-    print("Upload called", file=sys.stderr)
-    data = await request.form()
-    print(data)
-    bytes = await (data["file"].read())
-    print(bytes)
-    return predict_image_from_bytes(bytes)
-
-
-def predict_image_from_bytes(bytes):
-    img = open_image(BytesIO(bytes))
-    print("predict image from bytes", file=sys.stderr)
-    print(bytes, file=sys.stderr)
-
-    losses = img.predict(cat_learner)
-
-    pred_class, pred_idx, outputs = MODEL.predict(img)
-    print("predicted class is: " + pred_class, file=sys.stderr)
-
-    return JSONResponse({
-        "predictions": sorted(
-            zip(cat_learner.data.classes, map(float, losses)),
-            key=lambda p: p[1],
-            reverse=True
-        )
-    })
+@app.route("/form2")
+def form2():
+    print("Form 2 called")
+    return render_template('form2.html')
 
 
 @app.route('/api', methods=['GET'])
 def api():
     # Handle empty requests.
-    print('Request recieved!', file=sys.stderr)
-    print(request.json, file=sys.stderr)
-    print('args above!', file=sys.stderr)
-    print('request.json: ' + request.json, file=sys.stderr)
-    if not request.json:
-        return jsonify({'error': 'no request received'})
-
-    # Parse request args into feature array for prediction.
+    print('Request received!', file=sys.stderr)
+    #     print(request.json, file=sys.stderr)
+    #     print('args above!', file=sys.stderr)
+    #     print('request.json: ' + request.json, file=sys.stderr)
+    #     if not request.json:
+    #         return jsonify({'error': 'no request received'})
+    #
+    #     # Parse request args into feature array for prediction.
     x_list, missing_data = parse_args(request.json)
     x_array = np.array([x_list])
 
