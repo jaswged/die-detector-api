5a1fb447a747784b2a6a7ec0c0c8093f67e0d0c9
root <root@localhost.localdomain>
2020-01-11 22:24:53 -0600

    Remove form2 and unused app methods

--- app/app.py
+++ app/app.py
@@ -29,18 +29,6 @@ async def analyze(request):
     return jsonify(prediction)
 
 
-@app.route('/a')
-def a():
-    print(path)
-    time.sleep(5)
-    print(path)
-    img = open_image('C:/Users/Jason/Pictures/d12.jpg')
-    pred_class, pred_idx, outputs = learn.predict(img)
-    print(pred_class, file=sys.stderr)
-    print(pred_class)
-    return render_template('docs.html')
-
-
 @app.route('/upload')
 def upload():
     return render_template('image.html')
@@ -53,9 +41,7 @@ def uploader():
         print(f)
         print("type of f is:")
         print(type(f))
-        # f.save(secure_filename(f.filename))
 
-        #img_bytes = data['file'].read()
         img_bytes = f
         print(img_bytes.content_type)
         print(img_bytes.content_length)
@@ -93,7 +79,6 @@ async def classify(request):
 async def upload_star():
     print("upload_star called", file=sys.stderr)
     data = await request.form()
-    # request.form()
     print(data)
 
     # Get image as bytes
@@ -118,33 +103,6 @@ async def upload_star():
 def form():
     print("Form called")
     return render_template('form.html')
-
-
-@app.route("/form2")
-def form2():
-    print("Form 2 called")
-    return render_template('form2.html')
-
-
-@app.route('/api', methods=['GET'])
-def api():
-    # Handle empty requests.
-    print('Request received!', file=sys.stderr)
-    #     print(request.json, file=sys.stderr)
-    #     print('args above!', file=sys.stderr)
-    #     print('request.json: ' + request.json, file=sys.stderr)
-    #     if not request.json:
-    #         return jsonify({'error': 'no request received'})
-    #
-    #     # Parse request args into feature array for prediction.
-    x_list, missing_data = parse_args(request.json)
-    x_array = np.array([x_list])
-
-    # Predict on x_array and return JSON response.
-    estimate = int(MODEL.predict(x_array)[0])
-    response = dict(ESTIMATE=estimate, MISSING_DATA=missing_data)
-
-    return jsonify(response)
 
 
 if __name__ == '__main__':
